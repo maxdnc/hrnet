b49cf9f4fefe1c59edd4ff8eab30d6679b2190ca
Maxime Danic <maxime.danic@gmail.com>
2024-09-08 01:50:53 +0300

    refactor: Update Employees List page styling and navigation

--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -8,19 +8,21 @@ import { columns } from './columns.js';
 const page = () => {
   const { employees } = useEmployeeStore();
   return (
-    <div className="px-1 py-8">
-      <h1 className="text-4xl font-bold">Employee List</h1>
+    <div className="px-6 py-20">
+      <h1 className="text-4xl font-bold">Employees List</h1>
       <DataTable columns={columns} data={employees} />
 
-      <Link
-        href="/"
-        className="mt-4 text-indigo-400 font-medium inline-flex items-center gap-2 hover:underline cursor-pointer group"
-      >
-        <span className="transform transition-transform  group-hover:-translate-x-1 ">
-          <ArrowLeft size={16} color="#818cf8" />
-        </span>
-        <span>Home</span>
-      </Link>
+      <div className="flex items-center">
+        <Link
+          href="/"
+          className="mx-auto text-xl mt-4 text-indigo-400 font-medium inline-flex items-center gap-4 hover:underline cursor-pointer group"
+        >
+          <span className="transform transition-transform  group-hover:-translate-x-1 ">
+            <ArrowLeft size={22} color="#818cf8" />
+          </span>
+          <span>Home</span>
+        </Link>
+      </div>
     </div>
   );
 };
