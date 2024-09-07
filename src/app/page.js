52aedcf73fe783ad3a06662a0c84cbd2ffe20768
Maxime Danic <maxime.danic@gmail.com>
2024-09-07 22:15:39 +0300

    feat: Update Home page with HRnet branding

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,26 @@
 import Link from 'next/link';
 import EmployeeForm from './components/EmployeeForm';
+import { ArrowUpRight } from 'lucide-react';
 
 const Home = () => {
   return (
-    <div>
-      <EmployeeForm />
+    <div className="px-1 py-8">
+      <div className="flex items-center flex-col gap-4">
+        <h1 className="text-4xl font-bold">HRnet</h1>
+        <p className="text-gray-600">Welcome to HRnet</p>
+        <Link
+          href="/employees"
+          className="mt-4 text-indigo-400 font-medium inline-flex gap-1 hover:underline cursor-pointer group"
+        >
+          <span>View employees list</span>{' '}
+          <span className="transform transition-transform group-hover:-translate-y-1 group-hover:translate-x-1 ">
+            <ArrowUpRight size={16} color="#818cf8" />
+          </span>
+        </Link>
+        <h2 className="text-2xl font-bold mt-6">Create Employee</h2>
+      </div>
 
-      <Link href="/employees" className="mt-4 text-blue-600 hover:underline">
-        View Employees{' '}
-      </Link>
+      <EmployeeForm />
     </div>
   );
 };
