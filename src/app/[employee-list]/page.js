67eff1f22fd928d92ff73047613341f23eceb54f
Maxime Danic <maxime.danic@gmail.com>
2024-09-07 21:59:52 +0300

    feat: Add employee list to page component

--- src/app/[employee-list]/page.js
+++ src/app/[employee-list]/page.js
@@ -1,7 +1,23 @@
+'use client';
+import Link from 'next/link';
+import useEmployeeStore from '../store/useEmployeeStore';
+
 const page = () => {
+  const { employees } = useEmployeeStore();
   return (
     <div>
       <h1 className="text-4xl font-bold">Employee List</h1>
+      <ul>
+        {employees.map((employee, index) => (
+          <li key={index}>
+            {employee.firstName} {employee.lastName}
+          </li>
+        ))}
+      </ul>
+
+      <Link href="/" className="mt-4 text-blue-600 hover:underline">
+        Go back to Home
+      </Link>
     </div>
   );
 };
