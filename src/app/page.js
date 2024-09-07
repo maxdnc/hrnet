157936eea12988bb24afc0b014971d76150ba959
Maxime Danic <maxime.danic@gmail.com>
2024-09-07 21:59:59 +0300

    feat: Add link to view employees in Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import EmployeeForm from './components/EmployeeForm';
 
 const Home = () => {
   return (
     <div>
       <EmployeeForm />
+
+      <Link href="/employees" className="mt-4 text-blue-600 hover:underline">
+        View Employees{' '}
+      </Link>
     </div>
   );
 };
