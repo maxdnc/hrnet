f8af1996bd62060ba8ce8c731f0535287208405a
Maxime Danic <maxime.danic@gmail.com>
2024-09-08 21:26:54 +0300

    refactor: Add employee form modal for creating new employees

--- src/app/components/EmployeeForm.jsx
+++ src/app/components/EmployeeForm.jsx
@@ -9,12 +9,17 @@ import useFormStore from '../store/useFormStore.js';
 import useEmployeeStore from '../store/useEmployeeStore.js';
 import { validateEmployeeForm, isFormValid } from '../utils/formValidation';
 import { RotateCcw } from 'lucide-react';
+import { Modal } from 'maxime-component-modal';
 
 const EmployeeForm = () => {
   const { formData, setFormData, resetFormData } = useFormStore();
   const { addEmployee } = useEmployeeStore();
   const [errors, setErrors] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [employeeName, setEmployeeName] = useState('');
+
+  const closeModal = () => setIsModalOpen(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,6 +33,8 @@ const EmployeeForm = () => {
     const newErrors = validateEmployeeForm(formData);
     setErrors(newErrors);
     if (isFormValid(newErrors)) {
+      setEmployeeName(formData.firstName); // Store the employee's name
+      setIsModalOpen(true);
       addEmployee(formData);
       resetFormData();
       setErrors({});
@@ -41,104 +48,136 @@ const EmployeeForm = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="max-w-md mx-auto mt-4 p-6 bg-white rounded-lg shadow-lg"
-    >
-      <FormField
-        label="First Name"
-        name="firstName"
-        value={formData.firstName}
-        onChange={handleChange}
-        error={errors.firstName}
-      />
-      <FormField
-        label="Last Name"
-        name="lastName"
-        value={formData.lastName}
-        onChange={handleChange}
-        error={errors.lastName}
-      />
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto mt-4 p-6 bg-white rounded-lg shadow-lg"
+      >
+        <FormField
+          label="First Name"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+          error={errors.firstName}
+        />
+        <FormField
+          label="Last Name"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          error={errors.lastName}
+        />
 
-      <DatePicker
-        label="Date of Birth"
-        name="dateOfBirth"
-        value={formData.dateOfBirth}
-        onChange={handleChange}
-        error={errors.dateOfBirth}
-      />
-      <DatePicker
-        label="Start Date"
-        name="startDate"
-        value={formData.startDate}
-        onChange={handleChange}
-        error={errors.startDate}
-      />
+        <DatePicker
+          label="Date of Birth"
+          name="dateOfBirth"
+          value={formData.dateOfBirth}
+          onChange={handleChange}
+          error={errors.dateOfBirth}
+        />
+        <DatePicker
+          label="Start Date"
+          name="startDate"
+          value={formData.startDate}
+          onChange={handleChange}
+          error={errors.startDate}
+        />
 
-      <fieldset className="mb-4 border p-4">
-        <legend className="block text-sm font-medium text-gray-700">
-          Address
-        </legend>
-        <div className="mt-2 space-y-4">
-          <FormField
-            label="Street"
-            name="street"
-            value={formData.street}
-            onChange={handleChange}
-            error={errors.street}
+        <fieldset className="mb-4 border p-4">
+          <legend className="block text-sm font-medium text-gray-700">
+            Address
+          </legend>
+          <div className="mt-2 space-y-4">
+            <FormField
+              label="Street"
+              name="street"
+              value={formData.street}
+              onChange={handleChange}
+              error={errors.street}
+            />
+            <FormField
+              label="City"
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+              error={errors.city}
+            />
+            <FormField
+              label="State"
+              name="state"
+              type="select"
+              value={formData.state}
+              onChange={handleChange}
+              options={STATES}
+              placeholder="Select a state"
+              error={errors.state}
+            />
+            <FormField
+              label="Zip Code"
+              name="zipCode"
+              value={formData.zipCode}
+              onChange={handleChange}
+              error={errors.zipCode}
+            />
+          </div>
+        </fieldset>
+        <FormField
+          label="Department"
+          name="department"
+          type="select"
+          value={formData.department}
+          onChange={handleChange}
+          options={DEPARTMENTS}
+          placeholder="Select a department"
+          error={errors.department}
+        />
+        <div className="mt-6 flex gap-4 ">
+          <PrimaryButton
+            type="submit"
+            label={isLoading ? 'Saving...' : 'Save'}
+            className={'w-full'}
           />
-          <FormField
-            label="City"
-            name="city"
-            value={formData.city}
-            onChange={handleChange}
-            error={errors.city}
-          />
-          <FormField
-            label="State"
-            name="state"
-            type="select"
-            value={formData.state}
-            onChange={handleChange}
-            options={STATES}
-            placeholder="Select a state"
-            error={errors.state}
-          />
-          <FormField
-            label="Zip Code"
-            name="zipCode"
-            value={formData.zipCode}
-            onChange={handleChange}
-            error={errors.zipCode}
+          <PrimaryButton
+            type="button"
+            label="Reset"
+            className={'flex-none'}
+            variant={'outline'}
+            icon={<RotateCcw size={22} />}
+            onClick={handleReset}
           />
         </div>
-      </fieldset>
-      <FormField
-        label="Department"
-        name="department"
-        type="select"
-        value={formData.department}
-        onChange={handleChange}
-        options={DEPARTMENTS}
-        placeholder="Select a department"
-        error={errors.department}
-      />
-      <div className="mt-6 flex gap-4 ">
-        <PrimaryButton
-          type="submit"
-          label={isLoading ? 'Saving...' : 'Save'}
-          className={'w-full'}
-        />
-        <PrimaryButton
-          type="button"
-          label="Reset"
-          className={'flex-none'}
-          variant={'outline'}
-          icon={<RotateCcw size={22} />}
-          onClick={handleReset}
-        />
-      </div>
-    </form>
+      </form>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title="Employee Created !"
+        positionTitle="center"
+        titleFontColor="#1e1a58"
+        borderTitle={true}
+        insideBorderColor="#898b8f"
+        insideBorderWidth={1}
+        backgroundColor="#1e1a58"
+        backgroundOpacity={70}
+        modalMaxWidth="600px"
+        modalBackgroundColor="#EEF2FF"
+        modalBorderColor="#a3a3ad"
+        modalBorderWidth={1}
+        modalBorderRadius={22}
+        closeColor="#898b8f"
+        blurAmount={5}
+      >
+        <p className="text-neutral-500 text-center my-8">
+          Employee{' '}
+          <span className="font-bold text-indigo-500 capitalize">
+            {employeeName}
+          </span>{' '}
+          has been successfully added to the list.{' '}
+          <span aria-label="celebration" role="img">
+            🎉
+          </span>
+        </p>
+      </Modal>
+    </>
   );
 };
 
