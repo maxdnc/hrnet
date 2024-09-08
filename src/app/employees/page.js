e08a8c7355a554f5a447305d60c36fe97cea3a52
Maxime Danic <maxime.danic@gmail.com>
2024-09-08 15:28:43 +0300

    refactor: Update Employees List page styling and navigation

--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -4,24 +4,26 @@ import useEmployeeStore from '../store/useEmployeeStore.js';
 import { ArrowLeft } from 'lucide-react';
 import { DataTable } from './data-table.jsx';
 import { columns } from './columns.js';
+import { MOCK_DATA_EMPLOYEES } from '../mockData/mockDataEmployees';
 
 const page = () => {
   const { employees } = useEmployeeStore();
-  return (
-    <div className="px-6 py-20">
-      <h1 className="text-4xl font-bold">Employees List</h1>
-      <DataTable columns={columns} data={employees} />
 
-      <div className="flex items-center">
-        <Link
-          href="/"
-          className="mx-auto text-xl mt-4 text-indigo-400 font-medium inline-flex items-center gap-4 hover:underline cursor-pointer group"
-        >
-          <span className="transform transition-transform  group-hover:-translate-x-1 ">
-            <ArrowLeft size={22} color="#818cf8" />
-          </span>
-          <span>Home</span>
-        </Link>
+  return (
+    <div className="max-w-[1640px] mx-auto">
+      <Link
+        href="/"
+        className="mx-auto px-6 text-xl mt-8 text-indigo-400 font-medium inline-flex items-center gap-3 hover:underline cursor-pointer group"
+      >
+        <span className="transform transition-transform  group-hover:-translate-x-1 ">
+          <ArrowLeft size={22} color="#818cf8" />
+        </span>
+        <span>Home</span>
+      </Link>
+      <div className="px-6 py-16">
+        <h2 className="text-3xl font-bold">Employees List</h2>
+
+        <DataTable columns={columns} data={employees} />
       </div>
     </div>
   );
